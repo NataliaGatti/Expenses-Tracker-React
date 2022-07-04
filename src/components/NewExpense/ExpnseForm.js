b58412f7f3fa19cfb2b7c9b7f3a1c69ad9dccb23
Natalia Gatti <gattinati.ng@gmail.com>
2022-07-04 12:33:05 -0300

    Finished expenses tracker

--- src/components/NewExpense/ExpnseForm.js
+++ src/components/NewExpense/ExpnseForm.js
@@ -1,7 +1,7 @@
 import './ExpenseForm.css';
 import React, { useState } from 'react';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [ enteredTitle, setEnteredTitle ] = useState('');
   const [ enteredAmount, setEnteredAmount ] = useState('');
   const [ enteredDate, setEnteredDate ] = useState('');
@@ -22,11 +22,11 @@ const ExpenseForm = () => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
@@ -59,6 +59,7 @@ const ExpenseForm = () => {
       </div>
     </div>
     <div className='new-expense__actiones'>
+      <button type='button' onClick={props.onCancel}>Cancel</button>
       <button type='submit'>Add Expense</button>
     </div>
   </form>
